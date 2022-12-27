8220fe4cb539da5bbd1637377b81695187326a51
killmepalas <killmepalas@gmail.com>
2022-12-28 02:37:11 +0300

    проба пера. пока не то, что надо, но работает и ладно

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: init
+init:
+	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+	go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+	go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+	go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest
+	go install github.com/bradleyjkemp/grpc-tools/...@latest
+	go get github.com/Syfaro/telegram-bot-api@latest
+
+.PHONY: proto
+proto:
+	rm -fr pb
+	mkdir -p pb
+
+	protoc -I api/proto --go_out=pb --go_opt=paths=source_relative \
+           --go-grpc_out=pb --go-grpc_opt=require_unimplemented_servers=false,paths=source_relative \
+           --grpc-gateway_out=pb  --grpc-gateway_opt=logtostderr=true,paths=source_relative,generate_unbound_methods=true \
+           --openapiv2_out cmd/server/playground --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true,merge_file_name=currency --openapiv2_opt generate_unbound_methods=true \
+            currency.proto
---
+++ main/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
+)
+
+func main() {
+	// подключаемся к боту с помощью токена
+	bot, err := tgbotapi.NewBotAPI("5369772786:AAHFWGS48G8Q3F5HeNzrz3V0RqeAG7rP-bs")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// инициализируем канал, куда будут прилетать обновления от API
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	upd, _ := bot.GetUpdatesChan(ucfg)
+	// читаем обновления из канала
+	for {
+		select {
+		case update := <-upd:
+			// Пользователь, который написал боту
+			UserName := update.Message.From.UserName
+
+			// ID чата/диалога.
+			// Может быть идентификатором как чата с пользователем
+			// (тогда он равен UserID) так и публичного чата/канала
+			ChatID := update.Message.Chat.ID
+
+			// Текст сообщения
+			Text := update.Message.Text
+
+			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+
+			// Ответим пользователю его же сообщением
+			reply := Text
+			// Создаем сообщение
+			msg := tgbotapi.NewMessage(ChatID, reply)
+			// и отправляем его
+			bot.Send(msg)
+		}
+
+	}
+}
